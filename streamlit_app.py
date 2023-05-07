b4a3a549b181e8490471072560aeb0b4f50dff4c
Moka Express <119792288+exmokapress@users.noreply.github.com>
2023-05-07 18:39:11 +0200

    test by using datafram

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # streamlit_app.py
 
 import streamlit as st
+import numpy as np
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
@@ -23,10 +24,38 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
-rows = run_query("select cast(a as FLOAT64) as lat, cast(b as FLOAT64) as lon, extract(time from TIMESTAMP_SECONDS(cast(c as INT64))) as time_value from `iot_dataset.07_05` LIMIT 10")
+
+def run_query_df(query):
+    query_job = client.query(query)
+    df_query = query_job.result().to_dataframe()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    return df_query
+
 
 # Print results.
 st.write("Some wise words from Shakespeare:")
 
-for row in rows:
-    st.write(row['time_value'])
+
+#rows = run_query("select cast(a as FLOAT64) as lat, cast(b as FLOAT64) as lon, extract(time from TIMESTAMP_SECONDS(cast(c as INT64))) as time_value from `iot_dataset.07_05` limit 10")
+
+#for row in rows:
+#    st.write(row['time_value'])
+
+df_query = run_query_df("select cast(a as FLOAT64) as lat, cast(b as FLOAT64) as lon, extract(time from TIMESTAMP_SECONDS(cast(c as INT64))) as time_value from `iot_dataset.07_05` limit 10")
+for column in df_query:
+    st.write(df_query[column])
+
+
+# CALCULATE MIDPOINT FOR GIVEN SET OF DATA
+@st.cache_data
+def mpoint(lat, lon):
+    """
+    This function calculates the midpoint of a set of latitude and longitude coordinates, using the numpy.average()
+    function. It uses Streamlit's caching feature to cache the result, so it only needs to be computed once. It returns
+    a tuple containing the latitude and longitude of the midpoint.
+    """
+    return (np.average(lat), np.average(lon))
+
+
+#zoom_level = 12 # the map will be zoomed in to show the airport in detail
+#midpoint = mpoint(data["lat"], data["lon"]) # the map will be centered on the midpoint of all the data points
