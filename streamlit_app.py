876f2c81d5922b1149630c3c8f4b407b1ba9c7c9
Moka Express <119792288+exmokapress@users.noreply.github.com>
2023-05-28 16:19:16 +0200

    load big tables to dataframes; work with dateframes

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,29 @@ credentials = service_account.Credentials.from_service_account_info(
 client = bigquery.Client(credentials=credentials)
 
 
-st.set_page_config(layout="wide", page_title="SCL rowing log demo", page_icon=":boat:")
+# Create a reference to the World Cup dataset
+dataset_ref = client.dataset("iot_dataset", project="serene-foundry-379413")  # CHANGE WITH YOUR OWN DATASET (NOT TABLE) NAME AND PROJECT ID
 
+# API request - fetch the dataset
+dataset = client.get_dataset(dataset_ref)
+
+# List the tables in the dataset
+tables = list(client.list_tables(dataset))
+for table in tables:  
+    print(table.table_id)
+
+# Create a reference to the "Teams" table and fetch the table
+table_ref = dataset_ref.table("rides") # Make sure it matches the table name printed above (case sensitive)
+rides_table = client.get_table(table_ref)
+df_rides = client.list_rows(rides_table).to_dataframe()
+
+# Same for the "Players" table
+table_ref = dataset_ref.table("gps_rides") # Make sure it matches the table name printed above (case sensitive)
+gps_rides_table = client.get_table(table_ref)
+df_gps_rides = client.list_rows(gps_rides_table).to_dataframe()
+
+
+st.set_page_config(layout="wide", page_title="SCL rowing log demo", page_icon=":boat:")
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
@@ -87,20 +108,33 @@ def map(data, lat, lon, zoom):
 #df = pd.read_csv("bquxjob_346917b4_18823823f98.csv")
 #print(df.head())
 
-df = run_query_df("select * from `iot_dataset.gps_rides` where date_value = '2023-05-02' order by time_value")
-print(df.head())
 
-df_list = []
-for index, row in df.iterrows():
-  df_list.append([row["lon"], row["lat"]])
-  
+
+#df = run_query_df("select * from `iot_dataset.gps_rides` where date_value = '2023-05-02' order by time_value")
+#print(df.head())
+
+
+def get_ride_member():
+    df_rides[df_rides.member_name_=='Shanshan'].ride_date.sort_values(ascending=False)
+    print(df_rides.ride_date[0])
+    return df_gps_rides[df_gps_rides.date_value==df_rides.ride_date[0]].sort_values(by='time_value')
+
+def get_map_ride(df):
+    print(df.head())
+    df_list = []
+    for index, row in df.iterrows():
+        df_list.append([row["lon"], row["lat"]])
+
+    df_new = pd.DataFrame(columns=['path'])
+    df_new.at['0', 'path'] = df_list  
+    print(df_new.head())
+    
+    return df_new
+
+
+
+"""
 coor_length = len(df_list)
-
-df_new = pd.DataFrame(columns=['path'])
-df_new.at['0', 'path'] = df_list  
-print(df_new.head())
-
-
 
 coor_list = df_new.iloc[0]['path']
 
@@ -117,26 +151,10 @@ while j < coor_length:
 
 coor_distance = round(coor_distance, 2)
 print(coor_distance)
-
-
-#df_date = run_query_df("select ride_date from `iot_dataset.rides` where member_name_ = 'Shanshan' order by ride_date limit 1")
-q1 = """
-bq query 
---use_legacy_sql=false 
---parameter=name:STRING:Shanshan 
-select ride_date
-from `iot_dataset.rides`
-where member_name_ = name 
-order by ride_date desc limit 1
 """
 
-df_date = run_query_df(q1)
-ride_date = df_date.iloc[0,0]
-
-#rows = run_query("select cast(a as FLOAT64) as lat, cast(b as FLOAT64) as lon, extract(time from TIMESTAMP_SECONDS(cast(c as INT64))) as time_value from `iot_dataset.07_05` limit 10")
-
-#for row in rows:
-#    st.write(row['time_value'])
+#df_date = run_query_df("select ride_date from `iot_dataset.rides` where member_name_ = 'Shanshan' order by ride_date limit 1")
+#ride_date = df_date.iloc[0,0]
 
 
 # LAYING OUT THE TOP SECTION OF THE APP WITH 2 COLUMNS
@@ -157,7 +175,7 @@ row2_1, row2_2 = st.columns((2, 1)) # Size of columns 2, 1, 1, 1
 
 
 #df_query = run_query_df("select cast(a as FLOAT64) as lat, cast(b as FLOAT64) as lon, extract(time from TIMESTAMP_SECONDS(cast(c as INT64))) as time_value from `iot_dataset.07_05`")
-midpoint = mpoint(df["lat"], df["lon"]) # the map will be centered on the midpoint of all the data points
+#midpoint = mpoint(df["lat"], df["lon"]) # the map will be centered on the midpoint of all the data points
 #map(df, midpoint[0], midpoint[1], 11)
 #for column in df_query:
 #    st.write(df_query[column])
@@ -165,13 +183,16 @@ midpoint = mpoint(df["lat"], df["lon"]) # the map will be centered on the midpoi
 with row2_1:
     if member:
         st.write("Previous rowing of ", member)
+        df_ride_member = get_ride_member()
+        df_new = get_map_ride(df_ride_member)
+        midpoint = mpoint(df_ride_member["lat"], df_ride_member["lon"])
         map(df_new, midpoint[0], midpoint[1], 13)
         
 with row2_2:
     if member:
         col1, col2, col3 = st.columns(3)
-        col1.metric("Your distance", coor_distance)
-        col2.metric("Ride date", ride_date)
-        col3.metric("Humidity", "86%", "4%")
+        #col1.metric("Your distance", coor_distance)
+        #col2.metric("Ride date", ride_date)
+        #col3.metric("Humidity", "86%", "4%")
 #zoom_level = 12 # the map will be zoomed in to show the airport in detail
 #midpoint = mpoint(data["lat"], data["lon"]) # the map will be centered on the midpoint of all the data points
